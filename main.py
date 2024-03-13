e9cba5c01c62b6a02891440223157141a928b68f
Eaulmesse <theo.guerin01120@gmail.com>
2024-03-13 18:23:41 +0100

    add: register method

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import sqlite3
-
-from flask import Flask, render_template, current_app, g
+from flask import Flask, render_template, request, g, redirect, url_for
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
 
 DATABASE = 'database.db'
 
@@ -15,6 +9,7 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row  # Cela permet d'accéder aux colonnes par nom
     return db
 
 @app.teardown_appcontext
@@ -23,19 +18,34 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        db = get_db()
+        cursor = db.cursor()
+        
+        cursor.execute("INSERT INTO User (name, password) VALUES (?, ?)",
+                        [request.form['name'], request.form['password']])
+        db.commit()
+        return redirect(url_for('home'))
+        
+    return render_template("register.html")
 
 def init_db():
     with app.app_context():
         db = get_db()
         cursor = db.cursor()
-
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='User';")
         table_exists = cursor.fetchone()
-        
         if not table_exists:
             with app.open_resource('schema.sql', mode='r') as f:
                 db.cursor().executescript(f.read())
             db.commit()
 
-
 init_db()
+
+
